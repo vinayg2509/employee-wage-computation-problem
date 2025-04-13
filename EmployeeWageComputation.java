bb993d4b02adc15e4142dc549a3dae5e3ae2dd45
vinayg2509 <vinayg2509@gmail.com>
2025-04-13 22:34:09 +0530

    [Vinay] Updated : Updated a program by calculating a daily wage of an employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -9,31 +9,29 @@ class EmployeeWageComputation
 	public static void main(String[] args) 
 	{
 		// Displaying a welcome message
-		System.out.println("Welcome to Employee Wage Computation Problem!!");
-
+		System.out.println("Welcome to employee wage computation Problem!!");
+		// creating a new Scanner Object
 		Scanner sc = new Scanner(System.in);
-
-		// Input employee name
-		System.out.print("Enter an employee name: ");
+		System.out.println("Enter a employee Name : ");
 		String name = sc.nextLine();
-
-		// Generate random attendance (0 - Absent, 1 - Present)
+		// creating random object
 		Random random = new Random();
-		int attendance = random.nextInt(2); // generates 0 or 1
-
-		// Display attendance result
-		if (attendance == 0) 
-		{
-			System.out.println("Employee " + name + " is absent.");
-		} 
-		else 
-		{
-			System.out.println("Employee " + name + " is present.");
-		}
-
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int attendance = random.nextInt(2);
+		System.out.println(attendance);
+		// check if the employee is absent
+		if (attendance == 0) {
+			System.out.println("Employee " + name + " is absent!!");
+		} else
+			System.out.println("Employee " + name + " is presnt ");
 		sc.close();
-
-
+		// calculating daily wage
+		int dailyWage = wagePerHour * fullDayHours;
+		// Output of the daily wage
+		System.out.println("Daily Employee Wage is : " + dailyWage);
 	}
 
 }
+
+
