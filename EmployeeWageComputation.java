e05a08882680bc46342a64a2c67b78404aac4440
vinayg2509 <vinayg2509@gmail.com>
2025-04-14 18:22:33 +0530

    [Vinay]Updated : Calculated a wages till a condition of total working hours or days is reached for a month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,5 @@
 package employeewagecomputation;
 
-
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,39 +19,52 @@ public class EmployeeWageComputation {
         // part-time attributes half of a full day
         int partTimeHours = 4;
         // total working days per month.
-        int totalWorkingDays = 20;
+        int maxWorkingDays = 20;
+        // maximum working hours
+        int maxWorkingHours = 100;
         // initializing the total wage per month
         int wagePerMonth = 0;
-        for (int day = 1; day <= totalWorkingDays; day++) {
+        int totalHours = 0;
+        int totalDays = 0;
+        while (totalHours < maxWorkingHours && totalDays < maxWorkingDays) {
+            totalDays++;
             // 0 - absent , 1-present for full time , 2- present for part-time
             int attendance = random.nextInt(3);
             // solving in switch case instead of if-else to find the attendance status
             int dailyHours = 0;
             switch (attendance) {
                 case 1:
-                    System.out.println("Day " + day + " Employee " + name + " is Present for full-time");
+                    System.out.println("Day " + totalDays + " Employee " + name + " is Present for full-time");
                     // setting daily hours for full time
                     dailyHours = fullDayHours;
                     break;
                 case 2:
-                    System.out.println("Day " + day + " Employee " + name + " is Presnt for part-time");
+                    System.out.println("Day " + totalDays + " Employee " + name + " is Presnt for part-time");
                     // setting a daily hours for part time
                     dailyHours = partTimeHours;
                     break;
                 case 0:
-                    System.out.println("Day " + day + " Employee " + name + " is Absent!!");
+                    System.out.println("Day " + totalDays + " Employee " + name + " is Absent!!");
                     dailyHours = 0;
                     break;
                 default:
                     System.out.println("Invalid Attendance!");
             }
+            // If total hours exceed max limit after adding, restrict to maxWorkingHours
+            if (totalHours + dailyHours > maxWorkingHours) {
+                dailyHours = maxWorkingHours - totalHours;
+            }
+            totalHours += dailyHours;
             // calculate the daily wage
             int dailyWage = dailyHours * wagePerHour;
             // total wage based on month
             wagePerMonth += dailyWage;
-            System.out.println("Daily wage for Day " + day + ": " + dailyWage);
+            System.out.println("Daily wage for Day " + totalDays + ": " + dailyWage);
+            System.out.println("Total Hours Worked so far: " + totalHours);
+            System.out.println("--------------------------------------------");
         }
-        // Output of the daily wage
+        System.out.println("Total Days Worked: " + totalDays);
+        System.out.println("Total Hours Worked: " + totalHours);
         System.out.println("Total Wage for a month : " + wagePerMonth);
         sc.close();
     }
