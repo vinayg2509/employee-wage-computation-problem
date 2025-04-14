41102e20fb1ca0cfe2e36b564c2de56651ac3bb3
vinayg2509 <vinayg2509@gmail.com>
2025-04-14 08:32:35 +0530

    [Vinay] Modified : modified the problem by using the switch case instead of if-else

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 package employeewagecomputation;
 
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,24 +22,28 @@ public class EmployeeWageComputation {
         int dailyWage = 0;
         // 0 - absent , 1-present for full time , 2- present for part-time
         int attendance = random.nextInt(3);
-        System.out.println(attendance);
-        // check if the employee is absent
-        if (attendance == 1) {
-            System.out.println("Employee " + name + " is Present for full-time");
-            // calculating daily wage for full time
-            dailyWage = wagePerHour * fullDayHours;
-        } else if (attendance == 2) {
-            System.out.println("Employee " + name + " is Presnt for part-time");
-            // calculating the wage for part time
-            dailyWage = wagePerHour * partTimeHours;
-        } else {
-            System.out.println("Employee " + name + " is Absent!!");
-            dailyWage = 0;
+        // solving in switch case instead of if-else to find the attendance status
+        switch (attendance) {
+            case 1:
+                System.out.println("Employee " + name + " is Present for full-time");
+                // calculating daily wage for full time
+                dailyWage = wagePerHour * fullDayHours;
+                break;
+            case 2:
+                System.out.println("Employee " + name + " is Presnt for part-time");
+                // calculating the wage for part time
+                dailyWage = wagePerHour * partTimeHours;
+                break;
+            case 0:
+                System.out.println("Employee " + name + " is Absent!!");
+                dailyWage = 0;
+                break;
+            default:
+                System.out.println("Invalid Attendance!");
         }
-        sc.close();
-
         // Output of the daily wage
         System.out.println("Daily Employee Wage is : " + dailyWage);
+        sc.close();
     }
 
 }
